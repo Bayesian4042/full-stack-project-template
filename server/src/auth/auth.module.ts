53b687ae17118f8e7e86ec6c57b878d72a9bc3c4
Abhilasha <singhabhilasha4042@gmail.com>
2024-05-07 19:00:45 +0530

    adds auth module

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { UsersModule } from 'src/users/users.module';
+import { LocalStrategy } from './strategies/local.strategy';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
+  imports: [UsersModule, ConfigModule, JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.getOrThrow('JWT_SECRET'),
+        signOptions: { 
+          expiresIn: configService.getOrThrow('JWT_EXPIRATION') 
+        },
+      }),
+    inject: [ConfigService],
+    }
+  )],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService, LocalStrategy],
 })
 export class AuthModule {}
--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,4 +1,47 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { UsersService } from 'src/users/users.service';
+import * as bcrypt from 'bcrypt';
+import { User } from '@prisma/client';
+import { ConfigService } from '@nestjs/config';
+import ms from 'ms';
+import { TokenPayload } from './token-payload.interface';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(
+        private readonly usersService: UsersService,
+        private readonly configService: ConfigService,
+        private readonly jwtService: JwtService,
+    ) { }
+
+    async login(user: User, response: Response) {
+        const expires = new Date();
+
+        expires.setMilliseconds(expires.getMilliseconds() + 
+            ms(this.configService.get<string>('JWT_EXPIRATION')),
+        );
+
+        const tokenPayload: TokenPayload = {
+            userId: user.id,
+        }
+
+        const token = await this.jwtService.signAsync(tokenPayload);
+
+        return { access_token: token, expires: expires };
+    }
+
+    async verifyUser(email: string, password: string): Promise<any> {
+        try {
+            const user = await this.usersService.getUser({ email: email });
+            const authenticated = await bcrypt.compare(password, user?.password);
+            if (!authenticated) {
+                throw new UnauthorizedException("Credentials are invalid.");
+            }
+            return user;
+        } catch (error) {
+            throw new UnauthorizedException("Credentials are invalid.");
+        }
+    }
+        
+}
