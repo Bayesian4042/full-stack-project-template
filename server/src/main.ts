72e667b7e979ee7ade377ad0de8643b1ab9367bf
Abhilasha <singhabhilasha4042@gmail.com>
2024-05-09 23:23:07 +0530

    adds swagger ui

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,11 +3,22 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { Logger } from 'nestjs-pino';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useLogger(app.get(Logger));
   app.useGlobalPipes(new ValidationPipe({ whitelist: true}));
+
+  const config = new DocumentBuilder()
+    .setTitle('NestJS API')
+    .setDescription('API for NestJS')
+    .setVersion('1.0')
+    .addTag('API')
+    .build();
+  
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(app.get(ConfigService).getOrThrow('PORT'));
 }
 bootstrap();
