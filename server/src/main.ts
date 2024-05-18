ab77fd3923890a2ff0ec6a48dffb2c944d3635a5
Abhilasha <singhabhilasha4042@gmail.com>
2024-05-18 23:07:00 +0530

    updates main.ts

--- server/src/main.ts
+++ server/src/main.ts
@@ -8,7 +8,11 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useLogger(app.get(Logger));
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true}));
+  app.useGlobalPipes(new ValidationPipe({ 
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+  }));
 
   const config = new DocumentBuilder()
     .setTitle('NestJS API')
